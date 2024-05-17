2e370e1cc4b5b59172dcdc2156f2df56c0db6a58
Matchima Klomkasem <88373747+Mindo1@users.noreply.github.com>
2024-05-17 16:13:27 +0700

    🧪 update unit test

--- src/cats/cats.service.spec.ts
+++ src/cats/cats.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 describe('CatsService', () => {
   let service: CatsService;
@@ -12,7 +13,41 @@ describe('CatsService', () => {
     service = module.get<CatsService>(CatsService);
   });
 
-  it('should be defined', () => {
+  afterEach(() => {
+    // restore the spy created with spyOn
+    jest.restoreAllMocks();
+  });
+
+  it('should be defined service', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should be create new cat', () => {
+    const input = {
+      name: 'hoo',
+      age: 1,
+      breed: 'm',
+    };
+
+    const expected = [
+      {
+        name: 'hoo',
+        age: 1,
+        breed: 'm',
+      } as Cat
+    ];
+
+    jest.spyOn(service, 'create');
+    const result = service.create(input);
+    expect(service.create).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(expected);
+  });
+
+  it('should be get []', () => {
+    const expected = [];
+    jest.spyOn(service, 'findAll'); // mock service ที่อยากเรียก มี parameter ( <name_service> + <name_function_in_service> )
+    const result = service.findAll(); // call service
+    expect(service.findAll).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(expected);
   });
 });
